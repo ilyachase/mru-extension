6b792d4ef21a1008ed9eca8b6ac4dd25bce0069e
ilyachase <ilya.chase@yandex.ru>
2021-11-28 13:04:06 +0500

    Store tabs, sync the handlers

--- background.js
+++ background.js
@@ -1,4 +1,9 @@
-let tabs = [];
+let justRemoved = false;
+
+const wait = () =>
+    new Promise(resolve =>
+        setTimeout(() => resolve(true), 50)
+    );
 
 async function getCurrentTab() {
     let queryOptions = {active: true, currentWindow: true};
@@ -6,30 +11,55 @@ async function getCurrentTab() {
     return tab;
 }
 
-chrome.runtime.onInstalled.addListener(() => {
-    getCurrentTab().then(tabInfo => tabs.push(tabInfo.id));
+chrome.runtime.onInstalled.addListener(async () => {
+    let tabsHistory = [];
+    await getCurrentTab().then(tabInfo => tabsHistory.push(tabInfo.id));
+
+    await chrome.storage.local.set({tabsHistory});
 });
 
-chrome.tabs.onActivated.addListener(function (tabInfo) {
-    if (tabs[0] === tabInfo.tabId) {
+chrome.tabs.onActivated.addListener(async function (tabInfo) {
+    while (justRemoved) {
+        await wait();
+    }
+
+    let tabsHistory;
+    await chrome.storage.local.get('tabsHistory').then(result => tabsHistory = result.tabsHistory);
+
+    if (tabsHistory[0] === tabInfo.tabId) {
         return;
     }
 
-    tabs.unshift(tabInfo.tabId);
-    if (tabs.length > 100) {
-        tabs.pop();
+    tabsHistory.unshift(tabInfo.tabId);
+    if (tabsHistory.length > 100) {
+        tabsHistory.pop();
     }
+
+    await chrome.storage.local.set({tabsHistory});
 });
 
-chrome.tabs.onRemoved.addListener(function () {
-    tabs.shift();
+chrome.tabs.onRemoved.addListener(async function () {
+    justRemoved = true;
+
+    let tabsHistory;
+    await chrome.storage.local.get('tabsHistory').then(result => tabsHistory = result.tabsHistory);
+
+    tabsHistory.shift();
+
+    await chrome.storage.local.set({tabsHistory});
+    justRemoved = false;
 });
 
-chrome.commands.onCommand.addListener(() => {
-    if (tabs.length < 2) {
+chrome.commands.onCommand.addListener(async () => {
+    let tabsHistory;
+    await chrome.storage.local.get('tabsHistory').then(result => tabsHistory = result.tabsHistory);
+
+    if (tabsHistory.length < 2) {
         return;
     }
 
-    chrome.tabs.update(tabs[1], {highlighted: true, active: true});
-    [tabs[0], tabs[1]] = [tabs[1], tabs[0]];
+    await chrome.tabs.update(tabsHistory[1], {highlighted: true, active: true});
+    [tabsHistory[0], tabsHistory[1]] = [tabsHistory[1], tabsHistory[0]];
+
+    await chrome.storage.local.set({tabsHistory});
 });
