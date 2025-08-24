25f142bac9d157ba7234b2315de127513b75e4d6
Ilya Levin <ilya.chase@yandex.ru>
2025-08-24 13:03:16 +0500

    better infra (eslint + vite + ts)

--- eslint.config.js
+++ eslint.config.js
@@ -1,47 +1,71 @@
 import js from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
 
 export default [
-    js.configs.recommended,
-    {
-        ignores: ['node_modules/', '*.min.js']
+  js.configs.recommended,
+  {
+    ignores: ['node_modules/', '*.min.js', 'build/']
+  },
+  {
+    files: ['src/**/*.ts'],
+    languageOptions: {
+      parser: tsparser,
+      ecmaVersion: 'latest',
+      sourceType: 'module'
     },
-    {
-        files: ['**/*.js'],
-        languageOptions: {
-            ecmaVersion: 'latest',
-            sourceType: 'module',
-            globals: {
-                chrome: 'readonly',
-                console: 'readonly',
-                setTimeout: 'readonly',
-                Promise: 'readonly'
-            }
-        },
-        rules: {
-            'no-unused-vars': ['error', { 
-                argsIgnorePattern: '^_',
-                varsIgnorePattern: '^_',
-                ignoreRestSiblings: true,
-                caughtErrors: 'none'
-            }],
-            'no-console': ['warn', { allow: ['log', 'warn', 'error'] }],
-            'prefer-const': 'error',
-            'no-var': 'error',
-            'eqeqeq': ['error', 'always'],
-            'curly': ['error', 'all'],
-            'brace-style': ['error', '1tbs'],
-            'indent': ['error', 4],
-            'quotes': ['error', 'single'],
-            'semi': ['error', 'always'],
-            'comma-dangle': ['error', 'never']
-        }
+    plugins: {
+      '@typescript-eslint': tseslint
     },
-    {
-        files: ['eslint.config.js'],
-        languageOptions: {
-            globals: {
-                process: 'readonly'
-            }
-        }
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      '@typescript-eslint/no-unused-vars': ['error', { 
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        ignoreRestSiblings: true,
+        caughtErrors: 'none'
+      }],
+      'no-console': ['warn', { allow: ['log', 'warn', 'error'] }],
+      '@typescript-eslint/prefer-const': 'error',
+      'no-var': 'error',
+      'eqeqeq': ['error', 'always'],
+      'curly': ['error', 'all'],
+      'brace-style': ['error', '1tbs'],
+      'indent': ['error', 4],
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'always'],
+      'comma-dangle': ['error', 'never']
     }
+  },
+  {
+    files: ['*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        console: 'readonly',
+        setTimeout: 'readonly',
+        Promise: 'readonly',
+        process: 'readonly'
+      }
+    },
+    rules: {
+      'no-unused-vars': ['error', { 
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        ignoreRestSiblings: true,
+        caughtErrors: 'none'
+      }],
+      'no-console': ['warn', { allow: ['log', 'warn', 'error'] }],
+      'prefer-const': 'error',
+      'no-var': 'error',
+      'eqeqeq': ['error', 'always'],
+      'curly': ['error', 'all'],
+      'brace-style': ['error', '1tbs'],
+      'indent': ['error', 4],
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'always'],
+      'comma-dangle': ['error', 'never']
+    }
+  }
 ];
